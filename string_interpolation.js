376ea6e1429e9cb5bb5fdbc99eac4142dae84261
adityakumar44 <adityakumar8279@gmail.com>
2023-01-25 21:05:49 +0530

    remove string_interpolation.js y

--- string_interpolation.js
+++
@@ -1,7 +0,0 @@
-
-let firstname = "Aditya";
-let lastname = "kumar";
-// console.log(firstname,lastname);
-// let fullname = firstname + " " + lastname;
-let fullname= `${firstname} ${lastname}`;
-console.log(fullname); //string interpolation
