817997de665c0f486d11e71bac6d609b6a22e380
adityakumar44 <adityakumar8279@gmail.com>
2023-01-25 20:50:56 +0530

    new

---
+++ week1-RJ11-CipherSchools/string_interpolation.js
@@ -0,0 +1,7 @@
+
+let firstname = "Aditya";
+let lastname = "kumar";
+// console.log(firstname,lastname);
+// let fullname = firstname + " " + lastname;
+let fullname= `${firstname} ${lastname}`;
+console.log(fullname); //string interpolation
---
+++ week1-RJ11-CipherSchools/variable.js
@@ -0,0 +1,2 @@
+var name= "aditya";
+console.log(name);
